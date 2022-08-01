dc1137c930f6904ac3081e0b8ae83a2be7d727bc
Cristian Johany <crislm1322@gmail.com>
2022-07-31 23:16:55 -0500

    base converter with stack

---
+++ 15-base-converter-with-stack/base-converter.js
@@ -0,0 +1,74 @@
+class Stack {
+  constructor() {
+    this.count = 0;
+    this.items = {};
+  }
+  push(element) {
+    this.items[this.count] = element;
+    this.count++;
+  }
+  pop() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+    this.count--;
+    const result = this.items[this.count];
+    delete this.items[this.count];
+    return result;
+  }
+  peek() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+    return this.items[this.count - 1];
+  }
+  isEmpty() {
+    return this.count === 0;
+  }
+  size() {
+    return this.count;
+  }
+  clear() {
+    /* while (!this.isEmpty()) {
+    this.pop();
+  } */
+    this.items = {};
+    this.count = 0;
+  }
+  toString() {
+    if (this.isEmpty()) {
+      return '';
+    }
+    let objString = `${this.items[0]}`;
+    for (let i = 1; i < this.count; i++) {
+      objString = `${objString},${this.items[i]}`;
+    }
+    return objString;
+  }
+}
+
+function baseConverter(decNumber, base) {
+  const remStack = new Stack();
+  const digits = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let number = decNumber;
+  let rem;
+  let baseString = '';
+
+  if (!(base >= 2 && base <= 36)) {
+    return '';
+  }
+
+  while (number > 0) {
+    rem = Math.floor(number % base);
+    remStack.push(rem);
+    number = Math.floor(number / base);
+  }
+
+  while (!remStack.isEmpty()) {
+    baseString += digits[remStack.pop()];
+  }
+
+  return baseString;
+}
+
+console.log(baseConverter(100345, 16));
