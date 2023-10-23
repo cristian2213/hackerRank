2bd51116d7228403557b049651a8168e864f0881
cristian2213 <crislm1322@gmail.com>
2023-10-22 23:30:22 -0500

    Cube matrix

---
+++ 20-multi-dimensional-array/index.js
@@ -0,0 +1,28 @@
+// Create a 3x3 matrix.
+
+const cubeMatrix = [];
+
+/*
+i = row
+j = column
+z = depth
+ */
+
+for (let i = 0; i < 3; i++) {
+  cubeMatrix[i] = [];
+  for (let j = 0; j < 3; j++) {
+    cubeMatrix[i][j] = [];
+    for (let z = 0; z < 3; z++) {
+      cubeMatrix[i][j][z] = i + j + z;
+    }
+  }
+}
+
+console.log(cubeMatrix);
+/*
+[
+  [ [ 0, 1, 2 ], [ 1, 2, 3 ], [ 2, 3, 4 ] ],
+  [ [ 1, 2, 3 ], [ 2, 3, 4 ], [ 3, 4, 5 ] ],
+  [ [ 2, 3, 4 ], [ 3, 4, 5 ], [ 4, 5, 6 ] ]
+]
+*/
