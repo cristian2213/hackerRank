f27bf6f6af4288a1477abfe4ebf38fb4de666ecb
cristian2213 <crislm1322@gmail.com>
2023-11-07 06:32:29 -0500

    Circular Linked List was updated with a new method to delete an element on an index.

--- 28-linked-list/src/circular-linked-list.ts
+++ 28-linked-list/src/circular-linked-list.ts
@@ -1,7 +1,7 @@
 import { LinkedList, ListNode as Node } from "./linked-list";
 
 class CircularLinkedList extends LinkedList {
-  constructor(equalsFn) {
+  constructor(equalsFn?) {
     super(equalsFn);
   }
 
@@ -22,7 +22,7 @@ class CircularLinkedList extends LinkedList {
         current = this.getElementAt(this.size());
         // --- Update last element
         this.head = newNode;
-        current.need = this.head;
+        current.next = this.head;
       }
     } else {
       // Add on middle or last place
@@ -31,9 +31,53 @@ class CircularLinkedList extends LinkedList {
       previous.next = newNode;
     }
 
-    this.count;
+    this.count++;
     return true;
   }
 
-  removeAt(index) {}
+  removeAt(index) {
+    if (index < 0 || index >= this.count) return undefined;
+
+    let current = this.head;
+
+    // Delete the first position
+    if (index === 0) {
+      // Delete the only element on the first position
+      if (this.size() === 1) {
+        this.head = undefined;
+      } else {
+        // Delete the first position and update the tail to the new head or if there is only one element left then update the tail to itself.
+        const removed = this.head;
+        current = this.getElementAt(this.size());
+        this.head = this.head.next;
+        current.next = this.head;
+        current = removed;
+      }
+    } else {
+      // no need to update last element for circular list.
+      const previous = this.getElementAt(index - 1);
+      current = previous.next;
+      previous.next = current.next;
+    }
+
+    this.count--;
+    return current.element;
+  }
+
+  getElementAt(index: any) {
+    let node = this.head;
+    for (let i = 0; i < index && node != null; i++) {
+      node = node.next;
+    }
+
+    return node;
+  }
 }
+
+const circularLinkedList = new CircularLinkedList();
+console.log("insert:", circularLinkedList.insert("a", 0));
+console.log("insert:", circularLinkedList.insert("b", 1));
+console.log("insert:", circularLinkedList.insert("d", 2));
+console.log("insert:", circularLinkedList.insert("c", 2));
+console.log("items:", circularLinkedList.toString()); // a,b,c,d
+console.log("removeAt:", circularLinkedList.removeAt(3)); // d
